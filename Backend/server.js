83977d63c8d6f333589d9f0629a78dd447bbe534
Saakshi Navelkar <saakshinavelkar26@gmail.com>
2026-03-30 12:22:13 +0530

    fix: updated gemini model

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,7 @@ Rules:
   try {
 
     const response = await ai.models.generateContent({
-      model: "gemini-1.5-flash",
+      model: "gemini-1.5-flash-latest",
       contents: [{ role: "user", parts: [{ text: prompt }] }]
     });
 
